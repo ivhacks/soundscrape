15bd25ad69be122663f4b7a168d388e177c30b89
Iv Robinson <aerobinsoniv@gmail.com>
2025-06-16 00:28:20 -0500

    more metadata functions

--- file_metadata.py
+++ file_metadata.py
@@ -30,12 +30,74 @@ def set_year(filename: str, year: int):
 
 def get_year(filename: str) -> int:
     tag = stagger.read_tag(filename)
+    if not tag.date:
+        raise stagger.NoTagError("Date not found")
     return int(tag.date)
 
 
 def clear_year(filename: str):
     tag = stagger.read_tag(filename)
     tag.date = ""
+    tag.write(filename)
+
+
+def set_album_title(filename: str, album_title: str):
+    tag = _read_or_create_tag(filename)
+    tag["TALB"] = album_title
+    tag.write(filename)
+
+
+def get_album_title(filename: str) -> str:
+    tag = stagger.read_tag(filename)
+    try:
+        return tag["TALB"].text[0]
+    except KeyError:
+        raise stagger.NoTagError("TALB frame not found")
+
+
+def clear_album_title(filename: str):
+    tag = stagger.read_tag(filename)
+    del tag["TALB"]
+    tag.write(filename)
+
+
+def set_artist(filename: str, artist: str):
+    tag = _read_or_create_tag(filename)
+    tag["TPE1"] = artist
+    tag.write(filename)
+
+
+def get_artist(filename: str) -> str:
+    tag = stagger.read_tag(filename)
+    try:
+        return tag["TPE1"].text[0]
+    except KeyError:
+        raise stagger.NoTagError("TPE1 frame not found")
+
+
+def clear_artist(filename: str):
+    tag = stagger.read_tag(filename)
+    del tag["TPE1"]
+    tag.write(filename)
+
+
+def set_song_title(filename: str, song_title: str):
+    tag = _read_or_create_tag(filename)
+    tag["TIT2"] = song_title
+    tag.write(filename)
+
+
+def get_song_title(filename: str) -> str:
+    tag = stagger.read_tag(filename)
+    try:
+        return tag["TIT2"].text[0]
+    except KeyError:
+        raise stagger.NoTagError("TIT2 frame not found")
+
+
+def clear_song_title(filename: str):
+    tag = stagger.read_tag(filename)
+    del tag["TIT2"]
     tag.write(filename)
 
 
--- test/test_metadata.py
+++ test/test_metadata.py
@@ -31,3 +31,60 @@ class TestReadMetadata(TestCase):
     def test_read_album_artist(self):
         # test/test_files_read_metadata/2025.mp3
         self.assertEqual(2025, 2025)
+
+    def test_get_album_title(self):
+        set_album_title("test/yeet.mp3", "Test Album")
+        album_title = get_album_title("test/yeet.mp3")
+        self.assertEqual(album_title, "Test Album")
+
+    def test_set_album_title(self):
+        set_album_title("test/yeet.mp3", "Album One")
+        album_title = get_album_title("test/yeet.mp3")
+        self.assertEqual(album_title, "Album One")
+        set_album_title("test/yeet.mp3", "Album Two")
+        album_title = get_album_title("test/yeet.mp3")
+        self.assertEqual(album_title, "Album Two")
+
+    def test_clear_album_title(self):
+        clear_album_title("test/yeet.mp3")
+        with self.assertRaises(stagger.NoTagError):
+            get_album_title("test/yeet.mp3")
+        set_album_title("test/yeet.mp3", "Test Album")
+
+    def test_get_artist(self):
+        set_artist("test/yeet.mp3", "Test Artist")
+        artist = get_artist("test/yeet.mp3")
+        self.assertEqual(artist, "Test Artist")
+
+    def test_set_artist(self):
+        set_artist("test/yeet.mp3", "Artist One")
+        artist = get_artist("test/yeet.mp3")
+        self.assertEqual(artist, "Artist One")
+        set_artist("test/yeet.mp3", "Artist Two")
+        artist = get_artist("test/yeet.mp3")
+        self.assertEqual(artist, "Artist Two")
+
+    def test_clear_artist(self):
+        clear_artist("test/yeet.mp3")
+        with self.assertRaises(stagger.NoTagError):
+            get_artist("test/yeet.mp3")
+        set_artist("test/yeet.mp3", "Test Artist")
+
+    def test_get_song_title(self):
+        set_song_title("test/yeet.mp3", "Test Song")
+        song_title = get_song_title("test/yeet.mp3")
+        self.assertEqual(song_title, "Test Song")
+
+    def test_set_song_title(self):
+        set_song_title("test/yeet.mp3", "Song One")
+        song_title = get_song_title("test/yeet.mp3")
+        self.assertEqual(song_title, "Song One")
+        set_song_title("test/yeet.mp3", "Song Two")
+        song_title = get_song_title("test/yeet.mp3")
+        self.assertEqual(song_title, "Song Two")
+
+    def test_clear_song_title(self):
+        clear_song_title("test/yeet.mp3")
+        with self.assertRaises(stagger.NoTagError):
+            get_song_title("test/yeet.mp3")
+        set_song_title("test/yeet.mp3", "Test Song")

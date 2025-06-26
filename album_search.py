4289708819d472c5beab07833d0d47dfb2332467
Iv Robinson <aerobinsoniv@gmail.com>
2025-06-25 23:31:38 -0700

    Add Gemini-powered album search

---
+++ album_search.py
@@ -0,0 +1,85 @@
+from google import genai
+from google.genai import types
+from pydantic import BaseModel
+
+
+def search_prompt(artist: str, song_title: str) -> str:
+    return f"""What album is the song {artist} - {song_title} on, and what year was it released?
+            Note that the song may have been released as a standalone single and then later on an album.
+            for these songs, please try to search out and respond with the album and year that it was released on the album.
+            Some tracks may have already been released as singles, but are set to be re-released on upcoming albums.
+            Be careful to only respond with an album if it's already been released. 
+            If the album is not yet released, but the track has been released as a single, say 'single' and do not mention the album.
+            Respond as if you have no knowledge of the upcoming album, and are only aware of the already released single.
+            Be concise and brief, give no other information or context. Respond with a single sentence.
+            Example responses: 
+            'Bittersweet was released as a single in 2025.'
+            'fast n slow is on knock2's 2025 album nolimit.'"""
+
+
+class AlbumTemplate(BaseModel):
+    title: str
+    single: bool
+    year: int
+
+
+class Album:
+    title: str
+    single: bool
+    year: int
+
+    def __init__(self, title: str, single: bool, year: int):
+        self.title = title
+        self.single = single
+        self.year = year
+
+    def __str__(self):
+        if self.single:
+            return f"{self.title} (single) ({self.year})"
+        else:
+            return f"{self.title} ({self.year})"
+
+
+def identify_album(artist: str, song_title: str) -> str | None:
+    with open(".env", "r") as f:
+        gemini_api_key = f.read()
+
+    client = genai.Client(api_key=gemini_api_key)
+    grounding_tool = types.Tool(google_search=types.GoogleSearch())
+    config = types.GenerateContentConfig(
+        tools=[grounding_tool], thinking_config=types.ThinkingConfig(thinking_budget=0)
+    )
+
+    prompt = search_prompt(artist, song_title)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=config,
+    )
+    print(response.text)
+    prompt = f"""The following is a response to a query about what album the song {artist} - {song_title} is on.
+            Provide the album title, year, and whether it's a standalone single in the expected format.
+            In the title field, only give the base album title. Omit features, "(single)", etc.
+            --------------------------------------------------------------------------------------
+            {response.text}"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=AlbumTemplate,
+            thinking_config=types.ThinkingConfig(thinking_budget=0),
+        ),
+    )
+
+    song_title = response.parsed.title
+    single = response.parsed.single
+    year = response.parsed.year
+
+    return Album(song_title, single, year)
+
+
+if __name__ == "__main__":
+    print(identify_album("Kevin Gates", "2 Phones"))
---
+++ test/test_album_search.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+from album_search import identify_album
+from album_search import search_prompt
+from google import genai
+from google.genai import types
+
+
+class AlbumSearchTests(TestCase):
+    def test_knock2_fast_n_slow(self):
+        album = identify_album("knock2", "fast n slow")
+        self.assertEqual(album.title, "nolimit")
+        self.assertEqual(album.single, False)
+        self.assertEqual(album.year, 2025)
+
+    def test_audien_bittersweet(self):
+        album = identify_album("audien, shallou, rosie darling", "bittersweet")
+        self.assertEqual(album.title, "Bittersweet")
+        self.assertEqual(album.single, True)
+        self.assertEqual(album.year, 2025)
+
+    def test_kevin_gates_2_phones(self):
+        album = identify_album("Kevin Gates", "2 Phones")
+        self.assertEqual(album.title, "Islah")
+        self.assertEqual(album.single, False)
+        self.assertEqual(album.year, 2016)
+
+
+class PromptTests(TestCase):
+    def test_ignores_unreleased_albums(self):
+        """
+        The song bittersweet is on the album first love, which is not yet released.
+        The prompt should not mention first love.
+        """
+        prompt = search_prompt("audien, shallou, rosie darling", "bittersweet")
+
+        with open(".env", "r") as f:
+            gemini_api_key = f.read()
+
+        client = genai.Client(api_key=gemini_api_key)
+        grounding_tool = types.Tool(google_search=types.GoogleSearch())
+        config = types.GenerateContentConfig(
+            tools=[grounding_tool],
+            thinking_config=types.ThinkingConfig(thinking_budget=0),
+        )
+        for i in range(10):
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=prompt,
+                config=config,
+            )
+
+            self.assertNotIn("first love", response.text.lower())
